c62c2eea7afc62d3c9503786da369b68d2558f13
Michele <michele_wyss@students.unibe.ch>
2013-09-20 15:41:03 +0200

    added GLHalfedgeStructure

---
+++ framework/src/glWrapper/GLHalfedgeStructure.java
@@ -0,0 +1,22 @@
+package glWrapper;
+
+import openGL.gl.GLDisplayable;
+import openGL.gl.GLRenderer;
+import openGL.objects.Transformation;
+
+public class GLHalfedgeStructure extends GLDisplayable {
+
+	@Override
+	public int glRenderFlag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void loadAdditionalUniforms(GLRenderer glRenderContext,
+			Transformation mvMat) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
