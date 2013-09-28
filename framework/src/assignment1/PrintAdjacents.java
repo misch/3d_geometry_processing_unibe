7fc6412c8d3e566145c060446b58ed98230802fe
Michele <michele_wyss@students.unibe.ch>
2013-09-28 14:55:42 +0200

    print out adjacency information for vertices

--- framework/src/assignment1/PrintAdjacents.java
+++ framework/src/assignment1/PrintAdjacents.java
@@ -5,6 +5,8 @@ import glWrapper.GLWireframeMesh;
 import java.io.IOException;
 import java.util.Iterator;
 
+import meshes.Face;
+import meshes.HalfEdge;
 import meshes.HalfEdgeStructure;
 import meshes.Vertex;
 import meshes.WireframeMesh;
@@ -29,7 +31,46 @@ public class PrintAdjacents {
 		 
 		GLWireframeMesh glOneNeighborhood = new GLWireframeMesh(oneNeighborhood);
 		
-		// TODO
-		// print that shit out
+		Vertex v = hs.getVertices().get(3);
+		printIncidentEdges(v);
+		printAdjacentVertices(v);
+		printIncidentFaces(v);
+	}
+
+	private static void printIncidentFaces(Vertex v) {
+		System.out.println("Vertex: " + v.toString());
+		Iterator<Face> iter = v.iteratorVF();
+		System.out.println("Adjacent faces: \n");
+		if (!iter.hasNext())
+			System.out.println("No adjacent faces");
+		while( iter.hasNext()){
+			System.out.println(iter.next());
+		}
+		System.out.println("=====================================");
+	}
+
+	private static void printAdjacentVertices(Vertex v) {
+		System.out.println("Vertex: " + v.toString());
+		Iterator<Vertex> iter = v.iteratorVV();
+		System.out.println("Adjacent vertices: \n");
+		if (!iter.hasNext())
+			System.out.println("No adjacent vertices");
+		while( iter.hasNext()){
+			System.out.println(iter.next());
+		}
+		System.out.println("=====================================");		
+	}
+
+	private static void printIncidentEdges(Vertex v) {
+		System.out.println("Vertex: " + v.toString());
+		Iterator<HalfEdge> iter = v.iteratorVE();
+		System.out.println("Incident edges: \n");
+		if (!iter.hasNext())
+			System.out.println("No incident edges");
+		while( iter.hasNext()){
+			System.out.println(iter.next());
+		}
+		System.out.println("=====================================");
+		
 	}
 }
