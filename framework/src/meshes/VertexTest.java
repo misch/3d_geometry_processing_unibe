db8b54e48b110ea00bdc4c5c047f4ddb62adf62b
Michele <michele_wyss@students.unibe.ch>
2013-09-28 16:38:58 +0200

    added a first test :-)

---
+++ framework/src/meshes/VertexTest.java
@@ -0,0 +1,35 @@
+package meshes;
+
+import static org.junit.Assert.*;
+import meshes.exception.DanglingTriangleException;
+import meshes.exception.MeshNotOrientedException;
+import meshes.reader.ObjReader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VertexTest {
+
+	private HalfEdgeStructure hs;
+	@Before
+	public void setUp() throws Exception {
+		WireframeMesh m = ObjReader.read("./objs/oneNeighborhood.obj", true);
+		hs = new HalfEdgeStructure();
+		
+		try {
+			hs.init(m);
+		} catch (MeshNotOrientedException | DanglingTriangleException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	@Test
+	public void testGetValence() {
+		Vertex center = hs.getVertices().get(0);
+		
+		assertEquals(5, center.getValence());
+		assertEquals(3, hs.getVertices().get(2).getValence());
+	}
+
+}
