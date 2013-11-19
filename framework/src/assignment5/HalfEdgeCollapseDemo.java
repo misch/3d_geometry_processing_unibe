fd64155c16c2c5e13b12db4184fe295eb6b0e214
Michele <michele_wyss@students.unibe.ch>
2013-11-19 17:50:44 +0100

    snitched @panmari 's visualization thing for potential dead edges

--- framework/src/assignment5/HalfEdgeCollapseDemo.java
+++ framework/src/assignment5/HalfEdgeCollapseDemo.java
@@ -1,6 +1,13 @@
 package assignment5;
 
 import glWrapper.GLHalfEdgeStructure;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3f;
+
 import meshes.HalfEdge;
 import meshes.HalfEdgeStructure;
 import meshes.WireframeMesh;
@@ -9,26 +16,35 @@ import openGL.MyDisplay;
 
 public class HalfEdgeCollapseDemo {
 	public static void main(String args[]) throws Exception {
-		WireframeMesh wf = ObjReader.read("objs/bunny5k.obj", true);
+		WireframeMesh wf = ObjReader.read("objs/bunny_ear.obj", true);
 		HalfEdgeStructure hsToKill = new HalfEdgeStructure();
 		HalfEdgeStructure hsWillLive = new HalfEdgeStructure();
 
 		hsToKill.init(wf);
 		hsWillLive.init(wf);
 
+		
+		HalfEdge deadEdge = hsToKill.getHalfEdges().get(4);
+		//mark the halfedge on untouched object
+		ArrayList<Vector3f> color = new ArrayList<Vector3f>(Collections.nCopies(hsWillLive.getVertices().size(), new Vector3f(0,0,1)));
+		color.set(deadEdge.end().index, new Vector3f(1,0,0));
+		color.set(deadEdge.start().index, new Vector3f(1,0,1));
+		
+		
 		HalfEdgeCollapse collapse = new HalfEdgeCollapse(hsToKill);
-		for (int i = 0; i < 800; i += 4) {
-			HalfEdge deadEdge = hsToKill.getHalfEdges().get(602);
+//		for (int i = 0; i < hsToKill.getHalfEdges().size()/3; i += 4) {
+//			HalfEdge deadEdge = hsToKill.getHalfEdges().get(1);
 			collapse.collapseEdgeAndDelete(deadEdge);
-		}
+//		}
 
 		GLHalfEdgeStructure glHsKill = new GLHalfEdgeStructure(hsToKill);
 		GLHalfEdgeStructure glHsLive = new GLHalfEdgeStructure(hsWillLive);
-
+		glHsLive.add(color, "color");
+		
 		glHsKill.configurePreferredShader("shaders/trimesh_flat.vert",
 				"shaders/trimesh_flat.frag", "shaders/trimesh_flat.geom");
-		glHsLive.configurePreferredShader("shaders/trimesh_flat.vert",
-				"shaders/trimesh_flat.frag", "shaders/trimesh_flat.geom");
+		glHsLive.configurePreferredShader("shaders/trimesh_flatColor3f.vert",
+				"shaders/trimesh_flatColor3f.frag", "shaders/trimesh_flatColor3f.geom");
 
 		MyDisplay d = new MyDisplay();
 		d.addToDisplay(glHsKill);
